834f20cdcf2f24062292563ed77e13687a0e2be5
monirakibul <monirakibul@gmail.com>
2022-04-18 21:20:41 +0600

    blog added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -5,7 +5,7 @@ const Blog = (props) => {
     return (
         <div className='col-lg-4 col-sm-12 col-md-6'>
             <div className="card">
-                <div className="card-body">
+                <div className="card-body text-start">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
                 </div>
